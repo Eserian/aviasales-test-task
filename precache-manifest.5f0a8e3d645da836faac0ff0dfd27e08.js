cfcc9ca7bb78f1b2193bd13389fe7a899431d1d6
Eserian <41377148+Eserian@users.noreply.github.com>
2020-09-01 14:17:26 +0000

    Deploying to gh-pages from  @ feb7f529bb51c2345faa7c22e21b945f3f0cfe0f 🚀

---
+++ precache-manifest.5f0a8e3d645da836faac0ff0dfd27e08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d6c191dd40ccf0226daefc0f2a7d65",
+    "url": "/aviasales-test-task/index.html"
+  },
+  {
+    "revision": "767b6acbd742c6070e10",
+    "url": "/aviasales-test-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee06efa827592cd22dcf",
+    "url": "/aviasales-test-task/static/js/2.2c89d4b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aviasales-test-task/static/js/2.2c89d4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767b6acbd742c6070e10",
+    "url": "/aviasales-test-task/static/js/main.26c9d314.chunk.js"
+  },
+  {
+    "revision": "1136ccc245d125b93e19",
+    "url": "/aviasales-test-task/static/js/runtime-main.84b0ed7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aviasales-test-task/static/media/logo.5d5d9eef.svg"
+  }
+]);
