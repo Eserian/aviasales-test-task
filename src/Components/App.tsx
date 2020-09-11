ff6140553b55db22295d0abb752f588bdeb89c9e
eserian <eserian@mail.ru>
2020-09-11 20:44:56 +0300

    add ticket component

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -19,52 +19,9 @@ export type ticket = {
   segments: flight[]
 }
 
-const testTickets: ticket[] = [
-  {
-    price: 93332,
-    carrier: "EY",
-    segments: [
-      {
-        origin: "MOW",
-        destination: "HKT",
-        date: "2020-09-21T05:35:00.000Z",
-        stops: ["IST","HKG"],
-        duration: 1706
-      },
-      {
-        origin: "HKT",
-        destination: "MOW",
-        date: "2020-10-11T14:56:00.000Z",
-        stops: ["DXB"],
-        duration: 681
-      }
-    ]
-  },
-  {
-    price: 93332,
-    carrier: "S7",
-    segments: [
-      {
-        origin: "MOW",
-        destination: "HKT",
-        date: "2020-09-21T05:35:00.000Z",
-        stops: ["IST","HKG"],
-        duration: 1706
-      },
-      {
-        origin: "HKT",
-        destination: "MOW",
-        date: "2020-10-11T14:56:00.000Z",
-        stops: ["DXB"],
-        duration: 681
-      }
-    ]
-  }
-];
-
 const App: FC = () => {
 
-  const [tickets] = useState(testTickets);
+  const [tickets] = useState([]);
 
   return (
     <>
