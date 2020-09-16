a6c07facce8fea4309830648f483a30368890b1e
Eserian <41377148+Eserian@users.noreply.github.com>
2020-09-16 17:34:51 +0000

    Deploying to gh-pages from  @ 7b92b13bfe0a8b6ac7f9c4f0098eb423698f15e6 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aviasales-test-task/precache-manifest.85986acac50656e3dda3cad00d50d71f.js"
+  "/aviasales-test-task/precache-manifest.f94a25c978b2540c3f62865ba4c28bb6.js"
 );
 
 self.addEventListener('message', (event) => {
