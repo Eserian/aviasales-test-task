07449ca1b5bf5703b0bd65590f41310fc6a3e4c2
eserian <eserian@mail.ru>
2020-09-20 22:48:01 +0300

    fix

--- src/components/FlightCard/FlightCard.tsx
+++ src/components/FlightCard/FlightCard.tsx
@@ -3,7 +3,7 @@ import React, { FC } from 'react';
 import plural from 'plural-ru';
 import { format, addMinutes } from 'date-fns';
 import { Ticket } from '../App';
-import './fliightCard.css';
+import './flightCard.css';
 
 type FlightCardProps = {
   data: Ticket;
