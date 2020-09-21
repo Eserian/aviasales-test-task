a8574482f913d05fc32380b09385ecaeb1c0906a
eserian <eserian@mail.ru>
2020-09-21 17:30:34 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
-import React, { FunctionComponent } from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './header.css';
 
-export const Header: FunctionComponent = () => (
+export const Header: FC = () => (
   <header className="header">
     <img src={logo} alt="logo" />
   </header>
